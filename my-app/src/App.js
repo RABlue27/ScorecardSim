89b7598099ce3701e3ca733a3f77e180fa47366d
Alex@gdplace.com <Alex@gdplace.com>
2023-08-13 13:10:11 -0400

    allow users to modify text input

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
-import PropTypes from 'prop-types';
 import './App.css';
 
-const GridSquare = ({ text }) => {
+const GridSquare = ({ text, onChange }) => {
   const [borderState, setBorderState] = useState(0);
 
   const increaseBorder = () => {
@@ -27,43 +26,76 @@ const GridSquare = ({ text }) => {
 
   return (
     <div className="button-box">
-      <div className="square" style={squareStyle} >
-        <div className="square-content" >{text}</div>
+      <div className="square" style={squareStyle}>
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => onChange(e.target.value)}
+          className="square-content"
+        />
       </div>
-      <button onClick={increaseBorder}> ++ </button> 
-      <button onClick={decreaseBorder}> -- </button> 
+      <button onClick={increaseBorder}> ++ </button>
+      <button onClick={decreaseBorder}> -- </button>
     </div>
   );
 };
 
-const Lineup = ( { name }) => {
+
+const Lineup = ({ name, onChange }) => {
   return (
     <div>
-      <textarea>
-        {name}
-      </textarea>
+      <textarea value={name.value} onChange={onChange} />
     </div>
   );
 };
 
 function App() {
-  const gridData = [];
-
-  for (let i = 0; i < 10; i++) {
-    const row = [];
-    for (let j = 0; j < 10; j++) {
-      row.push(`Square ${i}-${j}`);
+  const [gridData, setGridData] = useState(() => {
+    const data = [];
+    for (let i = 0; i < 10; i++) {
+      const row = [];
+      for (let j = 0; j < 10; j++) {
+        row.push('');
+      }
+      data.push(row);
     }
-    gridData.push(row);
-  }
+    return data;
+  });
+
+  const [rowNames, setRowNames] = useState(() => {
+    const names = [];
+    for (let i = 0; i < 10; i++) {
+      names.push('');
+    }
+    return names;
+  });
+
+  const handleTextChange = (rowIndex, columnIndex, newText) => {
+    const newData = [...gridData];
+    newData[rowIndex][columnIndex] = newText;
+    setGridData(newData);
+  };
+
+  const handleRowNameChange = (index, newName) => {
+    const newRowNames = [...rowNames];
+    newRowNames[index] = newName;
+    setRowNames(newRowNames);
+  };
 
   return (
     <div className="grid-container">
       {gridData.map((row, rowIndex) => (
-        <div key={rowIndex} className="grid-row">    
-          <Lineup /> 
+        <div key={rowIndex} className="grid-row">
+          <Lineup
+            name={rowNames[rowIndex]}
+            onChange={(newName) => handleRowNameChange(rowIndex, newName)}
+          />
           {row.map((text, columnIndex) => (
-            <GridSquare key={columnIndex} text={text} />
+            <GridSquare
+              key={columnIndex}
+              text={text}
+              onChange={(newText) => handleTextChange(rowIndex, columnIndex, newText)}
+            />
           ))}
         </div>
       ))}
